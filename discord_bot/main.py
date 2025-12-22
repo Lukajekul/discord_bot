68061407199849825de9aacc76f2981ee695a265
SLEEPWELL <66638927+Lukajekul@users.noreply.github.com>
2025-12-22 22:00:59 +0000

    Update

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,18 +1,25 @@
 # bot.py
 import os
-
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+handler = logging.FileHandler(filename='doscord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
 
-@client.event
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print("We are ready to go in, {bot.user.name}")
 
-client.run(TOKEN)
+bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
 
 "https://realpython.com/how-to-make-a-discord-bot-python/"
+"https://www.youtube.com/watch?v=YD_N6Ffoojw"
